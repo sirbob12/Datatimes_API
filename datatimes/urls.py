b8d37ef4052e3d7f38a887ed1b29ed18bd34f433
bobby <sirbob12@gmail.com>
2024-09-18 15:45:23 +0100

    new commit

--- datatimes/urls.py
+++ datatimes/urls.py
@@ -21,5 +21,5 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('blog.urls')),
+    path('api/', include('datatimes-api.onrender.com')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
