abbfbca0b6fb9c4644fa3f352593a71b922fd7be
bobby <sirbob12@gmail.com>
2024-09-19 13:24:38 +0100

    new commit settings

--- datatimes/urls.py
+++ datatimes/urls.py
@@ -19,7 +19,13 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
+from django.http import JsonResponse
+
+def root_view(request):
+    return JsonResponse({"message": "API root"})
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('blog.urls')),
-] #+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+     path('', root_view),  # Root URL handler
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
